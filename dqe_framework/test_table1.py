866ef67ccd4159ebf1af005f53089216e93ba4d0
shree <nimbaneshree@gmail.com>
2026-02-02 11:04:30 +0530

    after adding spark

--- dqe_framework/test_table1.py
+++ dqe_framework/test_table1.py
@@ -1,5 +1,27 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, upper, struct
+
+
 def test_count():
-    assert 1 == 1
+
+
+    spark = SparkSession.builder.getOrCreate()
+
+    data = [
+        (1, ("john", ("mumbai", 400001))),
+        (2, ("ravi", ("pune", 411001)))
+    ]
+    schema = "id INT, person STRUCT<name:STRING, address:STRUCT<city:STRING, pincode:INT>>"
+
+    df = spark.createDataFrame(data, schema)
+    df.show(truncate=False)
+
+    df2 = df \
+        .withColumn(
+        "persona", struct(upper(col("person.name")).alias("name"), struct(col("person.address.pincode").alias("pincode")
+                                                                          ).alias("address")
+                          )
+    )
 
 
 
